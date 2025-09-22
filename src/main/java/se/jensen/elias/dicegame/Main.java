c93c895b76ed56c1752b2022ffff205f07473d2c
Lagerq2 <eliaslagerqvist@outlook.com>
2025-09-22 21:59:32 +0200

    Addering av metoder

--- src/main/java/se/jensen/elias/dicegame/Main.java
+++ src/main/java/se/jensen/elias/dicegame/Main.java
@@ -3,6 +3,6 @@ package se.jensen.elias.dicegame;
 public class Main {
     static void main(String[] args) {
         Menu menu = new Menu(); // Skapar en instans av klassen Meny
-        menu.menu(); // kör metoden menu från Menyklassen
+        menu.startMenu(); // kör metoden menu från Menyklassen
     }
 }
--- src/main/java/se/jensen/elias/dicegame/Menu.java
+++ src/main/java/se/jensen/elias/dicegame/Menu.java
@@ -5,9 +5,25 @@ import java.util.Scanner; // importerar Scanner så jag kan ta emot användar in
 public class Menu {
     Scanner scanner = new Scanner(System.in); // Skapar en instans av Scanner
 
-    public void menu() { // menyn som sköter input med mera
-        boolean go = true;
-        while (go) { // While loop som gör att man kan spela fler gångr
+    private Player createPlayer(int playerNumber){
+        Player player = new Player(); //Skapar player objekt
+        while (true) {
+            try {
+                System.out.println("Enter firstname player " + playerNumber + " : ");
+                player.setFirstName(scanner.nextLine());// Använder set metod
+                System.out.println("Enter lastname player " + playerNumber + " : ");
+                player.setLastName(scanner.nextLine());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("You have to enter first and last name! Try again.");
+            }
+        }
+        return player;
+    }
+
+    public void startMenu() { // menyn som sköter input med mera
+
+        while (true) { // While loop som gör att man kan spela fler gångr
             System.out.println(" ");
             System.out.println("Welcome to DiceGame!"); // Välkommnar
             System.out.println(" ");
@@ -22,38 +38,13 @@ public class Menu {
 
             switch (choice) { // En switch som kör koden utifrån valet ovan
                 case 1:
-                    Player player1 = new Player(); //Skapa player1 objekt/spelare 1
-                    while (true) {
-                        try {
-                            System.out.println("Enter firstname player 1: ");
-                            player1.setFirstName(scanner.nextLine());// Använder set metod
-                            System.out.println("Enter lastname player 1: ");
-                            player1.setLastName(scanner.nextLine());
-                            break;
-                        } catch (IllegalArgumentException e) {
-                            System.out.println("You have to enter first and last name! Try again.");
-                        }
-                    }
-                    // Spelar information
-                    Player player2 = new Player(); //Skapar player2 objekt/spelare 2
-                    while (true) {
-                        try {
-
-                            System.out.println("Enter firstname player 2: ");
-                            player2.setFirstName(scanner.nextLine()); // använder set metod
-                            System.out.println("Enter lastname player 2: ");
-                            player2.setLastName(scanner.nextLine());
-                            break;
-                        } catch (IllegalArgumentException e) {
-                            System.out.println("You have to enter first and last name! Try again.");
-                        }
-                    }
-                    Game game = new Game(); // Skapar en instans av klassen Game
-                    game.play(player1, player2); // kör spelet med objekten/spelarna
+                   Player player1 =  createPlayer(1);
+                   Player player2 =  createPlayer(2);
+                   Game game = new Game();
+                   game.play(player1, player2);
                     break;
                 case 2:
-                    go = false; // sätter loopens argument till false, vilket avslutar programmet
-                    break;
+                    return;
             }
         }
     }
